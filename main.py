16321a8bf5a6ae643b14d48b245116779512c8d5
Victrollia <vicasmail@gmail.com>
2021-04-27 18:59:22 -0400

    adjust sales tax on clothing depending on price limit, added condition now allowing refunds

--- main.py
+++ main.py
@@ -19,12 +19,20 @@ def __get_tax(state):
 
 
 def checkout(purchase: Products, state):
-    if purchase.type.lower() == 'wic eligible' or purchase.type.lower() == 'clothing':
-        total = purchase.unit_price
-        return total
+    if purchase.unit_price < 0:
+        print("We do not accept refunds!")
+        return ''
+    elif purchase.type.lower() == 'clothing':
+        if purchase.unit_price > 100 and state.upper() == 'ME':
+            return round(purchase.unit_price + (purchase.unit_price * __get_tax(state)), 2)
+        elif purchase.unit_price > 175 and state.upper() == 'MA':
+            return round(purchase.unit_price + (purchase.unit_price * __get_tax(state)), 2)
+        else:
+            return purchase.unit_price
+    elif purchase.type.lower() == 'wic eligible':
+        return purchase.unit_price
     elif state.upper() not in ('MA', 'ME', 'NH'):
         print("Please enter MA, ME or NH")
         return ''
     else:
-        total = round(purchase.unit_price + (purchase.unit_price * __get_tax(state)), 2)
-        return total
+        return round(purchase.unit_price + (purchase.unit_price * __get_tax(state)), 2)
--- tests/test_production.py
+++ tests/test_production.py
@@ -3,6 +3,8 @@ import main
 milk = main.Products('milk', 2.99, 'WIC Eligible')
 tee = main.Products('tee', 14.99, 'clothing')
 cigarettes = main.Products('marlboro', 11.49, 'everything else')
+coat = main.Products('coat', 199.99, 'clothing')
+coat_return = main.Products('coat', -199.99, 'clothing')
 
 
 # test to check if sales tax was applied correctly
@@ -11,9 +13,15 @@ def test_checkout():
     assert main.checkout(tee, 'NH') == 14.99
     assert main.checkout(cigarettes, 'MA') == round(11.49 + (11.49 * 0.0625), 2)
     assert main.checkout(cigarettes, 'ME') == round(11.49 + (11.49 * 0.055), 2)
+    assert main.checkout(coat, 'MA') == round(199.99 + (199.99 * 0.0625), 2)
+    assert main.checkout(coat, 'ME') == round(199.99 + (199.99 * 0.055), 2)
 
 
 # test scenarios where state is written in lower case or invalid state
 def test_states():
     assert main.checkout(cigarettes, 'ma') > 0
     assert type(main.checkout(cigarettes, 'boo')) == str  # should return an error string
+
+
+def test_refunds():
+    assert type(main.checkout(coat_return, 'MA')) == str
